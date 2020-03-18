7023c82923982b41a21946da781443d9587f3c45
Nikolai Toverud <nikolai.toverud@gmail.com>
2020-03-18 19:43:26 +0100

    Added logic for testing if the word is the same as included

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,9 +1,27 @@
-require 'open-uri'
-require 'json'
-
 class GamesController < ApplicationController
   def new
     @letters = Array.new(10) { ('A'..'Z').to_a.sample }
-    @time = Time.now
+  end
+
+  def score
+    @answer = params[:answer]
+    @letters = params[:letters]
+    @include = included?(@answer.downcase, @letters.downcase)
+  end
+
+  def included?(answer, letters)
+    grid = letters.chars
+    answer.chars.all? { |letter| answer.count(letter) <= grid.count(letter) }
   end
 end
+
+# PSU
+# Add methods that check the different contitions. Call them in 'score'
+# 1. The word can’t be built out of the original grid
+# params["word"]
+# Att a time and a score for each attempt
+
+
+
+# The word is valid according to the grid, but is not a valid English word
+# The word is valid according to the grid and is an English word
